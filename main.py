0a0714e52b92bee42d569b8dcef7049f4ed8db3a
Nathan Podevin <nathanpodevin1997@gmail.com>
2023-02-08 11:54:34 +0100

    Ajout de la fonction versement

--- main.py
+++ main.py
@@ -72,3 +72,16 @@ def RetirerCompte(idCompte):
     fichier.close()
 
 #RetirerCompte('3')
+
+def verserment():
+    fichier = open(r"//home//user//PycharmProjects//gestionCompteBancaire//compte.txt", "a")
+    idProprietaire = input("Saisir l'id propriétaire : ")
+    VER = input("Saisir le montant du versement : ")
+    for ligne in fichier:
+        L = ligne.split(";")
+        if L[1].strip() == idProprietaire:
+            L[2] = int(VER) + int(L[2])
+            fichier.write(L[2])
+    fichier.close()
+
+#verserment()
